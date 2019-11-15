42c6b8d68911036ca6510ce42b73ff43b5d74c4c
sujanay <sujanay@hotmail.com>
2019-11-14 20:55:16 -0600

    modified utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def main():
+    """main entry point"""
+    print("inside main function")
+
+
+if __name__=='__main__':
+    main()
